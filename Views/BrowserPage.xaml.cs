6e0c6c655c77cb59b5ef7c085a3a9457000f74f3
immanuelvanmeirhaeghe <immanuel.van.meirhaeghe@gmail.com>
2024-05-13 20:54:13 +0200

    Latest

--- Views/BrowserPage.xaml.cs
+++ Views/BrowserPage.xaml.cs
@@ -1,4 +1,5 @@
-using Browser = NoPopsUI.Maui.ViewModels.Browser;
+using CustomBrowser = NoPopsUI.Maui.ViewModels.Browser;
+using CustomControls = NoPopsUI.Maui.Controls;
 
 namespace NoPopsUI.Views;
 
@@ -7,13 +8,17 @@ public partial class BrowserPage : ContentPage
     public BrowserPage()
     {
         InitializeComponent();
-        if (BindingContext is Browser browser)
+        if (BindingContext is CustomBrowser browser)
         {
             CustomWebView.Navigating += browser.CustomWebViewNavigating;
+            ShellTitleViewPinchGestureRecognizer.PinchUpdated += browser.ShellTitleViewOnPinchedUpdated;
+            CustomControls.CustomWebView.Control = CustomWebView;
+
             WebsitePicker.SelectedIndexChanged += browser.WebsitePickerSelectedIndexChanged;
             WebsitePicker.SetBinding(Picker.ItemsSourceProperty, nameof(browser.WebsitePickerItemsSource));
             WebsitePicker.SetBinding(Picker.SelectedItemProperty, nameof(browser.WebsitePickerSelectedItem));
             WebsitePicker.ItemDisplayBinding = new Binding(nameof(browser.WebsitePickerSelectedItem.Host));
+            CustomControls.WebsitePicker.Control = WebsitePicker;
         }
     }
 }
--- Views/OptionsPage.xaml.cs
+++ Views/OptionsPage.xaml.cs
@@ -11,12 +11,14 @@ public partial class OptionsPage : ContentPage
         {
             AddWebsiteEntry.TextChanged += options.AddWebsiteEntryTextChanged;
             AddWebsiteEntry.SetBinding(Entry.TextProperty, nameof(options.AddWebsiteEntryText));
-            
+            CustomControls.AddWebsiteEntry.Control = AddWebsiteEntry;
+
             WebsitePicker.SelectedIndexChanged += options.WebsitePickerSelectedIndexChanged;
             WebsitePicker.SetBinding(Picker.ItemsSourceProperty, nameof(options.WebsitePickerItemsSource));
             WebsitePicker.SetBinding(Picker.SelectedItemProperty, nameof(options.WebsitePickerSelectedItem));
-            WebsitePicker.ItemDisplayBinding = new Binding(nameof(options.WebsitePickerSelectedItem.Host));
-           
+            WebsitePicker.ItemDisplayBinding = new Binding(nameof(options.WebsitePickerSelectedItem.Host));           
+            CustomControls.WebsitePicker.Control = WebsitePicker;
+
             UseDefaultBrowserCheckBox.CheckedChanged += options.CustomCheckBoxCheckedChanged;
             CustomControls.UseDefaultBrowserCheckBox.Control = UseDefaultBrowserCheckBox;
                         
